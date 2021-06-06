24fc3a7ec6bdf693152b5f45649f14ea6df0d9f4
Deborah-Jia <vermouth2016@gmail.com>
2021-06-06 19:41:30 +0200

    finish ARISMA prediction part

--- homework.R
+++ homework.R
@@ -9,14 +9,18 @@ library(plotly)
 library(gapminder)
 library(highcharter)
 library(ggiraph)
+library(scales)
+library(forecast)
+library(Metrics)
+library('prophet')
 ggthemr('pale')
 
 # registration data
-rg <- fread("~/Desktop/Mastering Product Analytics/registrations.csv") 
+rg <- fread("~/Desktop/Mastering-Product-Analytics/MPA/registrations.csv") 
 str(rg) # check the structure
 
 # activity data
-acvt <- fread("~/Desktop/Mastering Product Analytics/activity.csv")
+acvt <- fread("~/Desktop/Mastering-Product-Analytics/MPA/activity.csv")
 str(acvt) # check the structure
 
 # Task 1: Acquisition -----------------------------------------------------
@@ -24,41 +28,71 @@ str(acvt) # check the structure
 # – Plot the number of registrations in each month
 table(rg$registration_month) # there are a lot of numbers larger than 12.
 # change numbers to month names.
-rg[,month_rg2 := fifelse(registration_month <= 12, 
+rg[, Month := fifelse(registration_month <= 12, 
                           rep(month.abb, 2)[registration_month], 
                           paste0("2nd Year ", rep(month.abb, 2)[registration_month]))]
 
 # sorting bars by factor ordering
-rg$month_rg2 <- factor(rg$month_rg2,levels = unique((rg$month_rg2)))
+rg$Month <- factor(rg$Month,levels = unique((rg$Month)))
 
-# plot the bar chart and add text above each bar
-p <- rg[, .(.N), by=month_rg2][, yoy := (N - lag(N, 12)) / lag(N, 12)] %>% 
-  ggplot(aes(label = month_rg2, label2 = N))  +
-  geom_bar(aes(x=month_rg2, y=N, group = 1),stat="identity")+
-  geom_line(aes(x=month_rg2, y=N*1.2, group = 1)) +
-  # geom_text(aes(label=N, x=month_rg2, y=N), vjust= -2) +
-  labs(title = "Registration in each month") +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title.y=element_blank()) +
-  geom_bar_interactive() 
+# group the registration and create a new data.table to save the result
+agg_rg <- rg[, .(.N), by=Month][, yoy := round((N - lag(N, 12))/ lag(N, 12), digits = 3) ]
 
-ggplotly(p) 
+# plot registration numbers and year over year ratio
+highchart() %>% 
+  hc_yAxis_multiples(
+    list(lineWidth = 3,title = list(text = "Registration")),
+    list(showLastLabel = F, opposite = T, title = list(text = "yoy rate")))%>% 
+  hc_add_series(data = agg_rg, hcaes(x = Month, y = N),type = "column", yAxis = 0) %>% 
+  hc_add_series(data = agg_rg, hcaes(x = Month, y = yoy),color = "red",type = "spline", yAxis = 1) %>%
+  hc_tooltip(
+    useHTML = TRUE,                            
+    formatter = JS(
+      "
+      function(){
+        outHTML = '<b>' + this.point.Month + '</b> <br> Registration ' + this.point.N + '<br> Year over year ratio ' + this.point.yoy*100 + '%'
+        return(outHTML)
+      }
 
-rg[, .(.N), by=month_rg2][, yoy := (N - lag(N, 12)) / lag(N, 12)] %>% 
-  highcharter::hw_grid(
-    hchart("column", hcaes(x = month_rg2, y = N)),
-    hchart("line", hcaes(x = month_rg2, y = N))
+      "
+    ),
+    shape = "square", 
+    borderWidth = 0   
   ) %>% 
-  hc_title(text = "Registration in Each Month") %>% 
-  hc_add_theme(hc_theme_chalk())
+  hc_xAxis(categories = agg_rg$Month ) %>% 
+  hc_legend(enabled = F) %>% 
+  hc_title(text = "Registration & Year over Year Ratio in Each Month") 
 
-
+  
 # – Make some comments on the trends and any lower periods (remember that Month 1 is January)
 #  每6个月有一次高峰和谷底
 
 # Extra tasks
 # – T1A: Calculate year-over-year growth of registrations (i.e. percentage increase or decline of Month 13 over Month 1). What would you project for Month 22?
+r22g <- rg[, .(.N), by=registration_month]
+# splitting data into train and valid sets
+## 75% of the sample size
+# splitting data into train and valid sets
+train = r22g[1:15,]
+valid = r22g[16:21,]
+# removing "Month" column
+train$registration_month = NULL
+# training model
+model = auto.arima(train)
+
+# model summary
+summary(model)
+
+# forecasting
+forecast = predict(model,6)
+
+# evaluation
+rmse(valid$N, forecast$pred)
+
 
 # – T1B: Do you see any regional difference in registration trends? Which geography is likely to drive future growth in registrations?
+
+
+
+
+
